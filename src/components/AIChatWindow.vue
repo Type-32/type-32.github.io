50ea3949143149cf8801a8f6a9733107d369793e
Type-32 <87076491+Type-32@users.noreply.github.com>
2023-12-20 01:32:59 +0800

    Env Impl 3

--- src/components/AIChatWindow.vue
+++ src/components/AIChatWindow.vue
@@ -3,6 +3,7 @@ import { ref, reactive, onMounted } from 'vue'
 import {OpenAI} from "openai";
 import {darkTheme, NButton, NCard, NConfigProvider, NInput, NInputGroup, NScrollbar, NSpin, useMessage} from "naive-ui";
 import VueMarkdown from "vue-markdown-render/src/VueMarkdown";
+import * as process from "process";
 
 interface ChatMessage {
     role: 'user' | 'assistant' | 'system';
@@ -13,13 +14,13 @@ const message = useMessage();
 const userInput = ref<string>('');
 const chatMessages = ref<ChatMessage[]>([]);
 const jwtMessages = reactive<ChatMessage[]>([{ role: "system", content: `You are a psychology therapist that helps patients who has distress in maintaining a well and stable mental health. You are to provide proper advice to your patient, whom you're talking to in this conversation, and give extra resources to help the patient. You can respond in markdown format. However, you are limited to only 600 characters per response, so limit your responses under that count. If the user asks for more details and explanations, you can respond to under 1000 characters.` }]);
-const apiKey = import.meta.env.VITE_OPENAI_API_KEY; // Ensure the key name starts with VITE_ to be exposed to your app
+const apiKey = process.env.VITE_OPENAI_API_KEY; // Ensure the key name starts with VITE_ to be exposed to your app
 const loading = ref(false)
-const allowActions = ref(true)
+const allowActions = ref(false)
 
 onMounted(() => {
     if (!apiKey) {
-        allowActions.value = false;
+        allowActions.value = true;
         console.error('OpenAI API key is not set in the environment variables');
         message.error('OpenAI API key is missing. Please set it in the environment variables.');
     }
