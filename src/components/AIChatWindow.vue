7c21b9b9681f6d5f0699d4b74915c9e26cd1cadf
Type-32 <87076491+Type-32@users.noreply.github.com>
2023-12-20 01:08:45 +0800

    Adjustments

--- src/components/AIChatWindow.vue
+++ src/components/AIChatWindow.vue
@@ -15,16 +15,22 @@ const chatMessages = ref<ChatMessage[]>([]);
 const jwtMessages = reactive<ChatMessage[]>([{ role: "system", content: `You are a psychology therapist that helps patients who has distress in maintaining a well and stable mental health. You are to provide proper advice to your patient, whom you're talking to in this conversation, and give extra resources to help the patient. You can respond in markdown format. However, you are limited to only 600 characters per response, so limit your responses under that count. If the user asks for more details and explanations, you can respond to under 1000 characters.` }]);
 const apiKey = import.meta.env.VITE_OPENAI_API_KEY; // Ensure the key name starts with VITE_ to be exposed to your app
 const loading = ref(false)
+const allowActions = ref(true)
 
 onMounted(() => {
     if (!apiKey) {
+        allowActions.value = false;
         console.error('OpenAI API key is not set in the environment variables');
         message.error('OpenAI API key is missing. Please set it in the environment variables.');
     }
 })
 // console.log(apiKey)
-const $openai = new OpenAI({apiKey, dangerouslyAllowBrowser: true});
-
+let $openai: any = null;
+try {
+    $openai = new OpenAI({apiKey, dangerouslyAllowBrowser: true});
+} catch (e) {
+    allowActions.value = false
+}
 async function askQuestion(questionContent: string): Promise<void> {
     loading.value = true;
     const questionMessage: ChatMessage  = { role: 'user', content: questionContent };
@@ -74,9 +80,9 @@ function handleFormSubmit() {
                     The AI is responding to your issues... Hang in there! You got this!
                 </template>
                 <NInputGroup class="flex flex-row">
-                    <NInput v-model:value="userInput" class="flex flex-grow text-left" placeholder="Tell me your issues..." type="textarea"/>
+                    <NInput :disabled="allowActions" v-model:value="userInput" class="flex flex-grow text-left" placeholder="Tell me your issues..." type="textarea"/>
                     <div class="flex-grow flex-row">
-                        <NButton @click="handleFormSubmit" class="flex-row h-full" strong>Ask</NButton>
+                        <NButton :disabled="allowActions" @click="handleFormSubmit" class="flex-row h-full" strong>Ask</NButton>
                     </div>
                 </NInputGroup>
                 <NScrollbar class="max-h-96 w-full">
