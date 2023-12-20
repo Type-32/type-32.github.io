04d0c5ba2c3fa5f7ba87e5990741c77a1da58421
Type-32 <87076491+Type-32@users.noreply.github.com>
2023-12-20 13:34:57 +0800

    Import Meta env.d.ts

--- env.d.ts
+++ env.d.ts
@@ -1,2 +1,8 @@
 /// <reference types="vite/client" />
 declare module 'markdown-it';
+interface ImportMetaEnv{
+    readonly VITE_OPENAI_API_KEY: string
+}
+interface ImportMeta{
+    readonly env: ImportMetaEnv
+}
